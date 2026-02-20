083bb2cb1df688c1d3e9cba148e3bc48d1611acc
srujalyt <srujalhawelikar07@gmail.com>
2026-02-20 18:38:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 # AIM: Task List Manager
 # Coder:
 # Date:
-
-print("--- Task List Manager ---")
 tasks = ["Sleep", "Getup", "Brush"]
 print(f"Original Tasks: {tasks}")
-
-# Write your code here
-# TODO: Add & Print new Task from user
-
-# TODO: Edit & Print task selected by User
-
-# TODO: Remove & Print a Task selected by User
-
-# TODO: Sort & Print the Tasks
+tasks.append(input())
+print(f"Tasks after Adding: {tasks}")
+edit_index = int(input())
+tasks[edit_index] = input()
+print(f"Tasks after Editing: {tasks}")
+tasks.pop(0)
+print(f"Tasks after Removing: {tasks}")
+tasks.sort()
+print(f"Tasks after Sorting: {tasks}")
